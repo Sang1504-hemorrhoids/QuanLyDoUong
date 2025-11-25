5c9f47767293b1801aead71f660132752a956a9a
Sang1504-hemorrhoids <sangtrandinh1504@gmail.com>
2025-11-25 18:36:36 +0700

    update crud

--- src/main/java/quanly/douong/dao/CrudDAO.java
+++ src/main/java/quanly/douong/dao/CrudDAO.java
@@ -1,10 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package quanly.douong.dao;
 
-public class CrudDAO {
+import java.util.List;
 
+public interface CrudDAO<T, ID> {
+    T create (T entity);
+
+    void update(T entity);
+
+    void deleteById(ID id);
+
+    List<T> findAll();
+
+    T findById(ID id);
 }
